03fa0cb396efb2b4a316c0a1143952b4b93dc706
Hanano Kobayashi <hannnk22@gmail.com>
2023-03-06 17:29:34 +0900

    :smailey_cat:update!!

--- blog/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.post_list, name='post_list'), #サイトにアクセスしたら「views.post_list」が正しい行き先だということをDjangoに伝える。「name='post_list'」はビューを識別するために使われるURLの名前
-]
