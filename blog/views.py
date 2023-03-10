6d352821cdc5018dd2a04532dd58ae0db07f79b6
Hanano Kobayashi <hannnk22@gmail.com>
2023-03-10 15:52:48 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
-<<<<<<< HEAD
 def post_list(request):
     return render(request,'blog/post_list.html',{})
-=======
->>>>>>> 03fa0cb396efb2b4a316c0a1143952b4b93dc706
